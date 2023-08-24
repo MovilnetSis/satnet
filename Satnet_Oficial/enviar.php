6e88915f626fb02e0f9b6e81c12f2143a32188ac
MovilnetSis <138257277+MovilnetSis@users.noreply.github.com>
2023-08-24 09:16:54 -0600

    satnet creado

---
+++ Satnet_Oficial/enviar.php
@@ -0,0 +1,28 @@
+
+<?php
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'rayda.pillaca@movilnet.pe';
+$asunto = 'Información ';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
+
+
+
